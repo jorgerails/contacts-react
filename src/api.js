4a1a767937c890d98204fe661018efe7104a53ce
Jorge Peris <jorgeperis@gmail.com>
2021-02-27 13:27:54 +0100

    Use .env for api key

--- src/api.js
+++ src/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: `http://localhost:3000`,
-  headers: {'accept': 'application/vnd.contacts+json', 'X-API-KEY': 'any-api-key'}
+  headers: {'accept': 'application/vnd.contacts+json', 'X-API-KEY': process.env.REACT_APP_API_KEY}
 });
